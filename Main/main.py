114bab66a17d17fc4b7fe8c4d3004a3b47f240e1
Aiman Najjar <najjar.aiman86@gmail.com>
2012-11-29 14:26:00 -0500

    output and interestingness cutoff

--- Main/main.py
+++ Main/main.py
@@ -47,22 +47,28 @@ if __name__ == '__main__':
 
     (largesets, supportCounts) = AprioriTID.aprioriTid(transactions, float(arglist[2]), float(arglist[3]))
 
-    print ""
-    print "==Large itemsets (min_sup=%%%d)" % (float(arglist[2])*100)
+
+
+    
+    output = "==Large itemsets (min_sup=%%%d)\n" % (float(arglist[2])*100)
     for largeset in largesets:
         itemset = largeset[0]
         if type(itemset) is not list:
             itemset = [itemset]
         
-        print "%s, %%%d" % (itemset, (float(largeset[1]) / len(transactions) * 100))
+        output +="%s, %%%d\n" % (itemset, (float(largeset[1]) / len(transactions) * 100))
 
-    print ""
-    print "==High-confidence association rules (min_conf=%%%d)" % (float(arglist[3]) * 100)
+    output +="\n"
+    output +="==High-confidence association rules (min_conf=%%%d)\n" % (float(arglist[3]) * 100)
 
+
+    rules = []
     for largeset in largesets:
         itemset = largeset[0]
         if type(itemset) is not list:
             continue
+        
+
         
         for n in map(list, itertools.permutations(itemset, len(itemset))):
 
@@ -85,15 +91,35 @@ if __name__ == '__main__':
 
             conviction = (1.0 - rhs_supp) / (1.0 - aconf)
 
+            if conviction < 50:
+                continue
+
             # interestingness(a->b)=sup(a,b)/(sup(a)sup(b))
             interestingness = support / (lhs_supp * rhs_supp)
 
             if confidence < float(arglist[3]):
                 continue
 
-            print "%s => %s (Conf %%%d, Supp: %%%d, conviction %f, interestingness %f) " % \
-                (n[0:len(n)-1],n[len(n)-1:len(n)], (confidence*100), (support*100), conviction, interestingness)
 
+            rules.append(tuple(("%s => %s" % (n[0:len(n)-1],n[len(n)-1:len(n)]), confidence*100, support*100, conviction, interestingness) ))
+
+    rules.sort(key=lambda rule: rule[3])
+
+    debug_output = output
+    for rule in rules:   
+
+        debug_output+="%s(Conf %%%d, Supp: %%%d, Conviction %f, Interestingness %f) \n" % \
+            (rule[0], rule[1], rule[2], rule[3], rule[4])
+
+        output+="%s(Conf %%%d, Supp: %%%d) \n" % \
+            (rule[0], rule[1], rule[2])
+            
+
+    print debug_output
+
+    f = open("output.txt", "w")
+    f.write(output)
+    f.close()
 
 
     
